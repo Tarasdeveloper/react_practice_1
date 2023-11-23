b53fe16584e917c6763459655b4c9b6123de7657
Tarasdeveloper <107717399+Tarasdeveloper@users.noreply.github.com>
2023-11-23 12:30:50 +0000

    Deploying to gh-pages from @ Tarasdeveloper/react_practice_1@51c594b813ba45145dc63c2755f4742680a83e1f 🚀

--- src/components/FriendListItem/FriendListItem.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Online = styled.span`
-  display: block;
-  width: 20px;
-  height: 20px;
-  border-radius: 50%;
-  background-color: ${(props) => (props.status ? 'green' : 'aqua')};
-`;
